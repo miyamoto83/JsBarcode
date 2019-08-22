5b7c6978e50b29061efdd0c4ba72b86763d56e56
Taku Miyamoto <miyamoto@entap.co.jp>
2019-08-22 12:33:15 +0900

    Fix EAN13 Barcode with text.

--- src/renderers/shared.js
+++ src/renderers/shared.js
@@ -8,17 +8,18 @@ function getEncodingHeight(encoding, options){
 }
 
 function getBarcodePadding(textWidth, barcodeWidth, options){
-	if(options.displayValue && barcodeWidth < textWidth){
-		if(options.textAlign == "center"){
-			return Math.floor((textWidth - barcodeWidth) / 2);
-		}
-		else if(options.textAlign == "left"){
-			return 0;
-		}
-		else if(options.textAlign == "right"){
-			return Math.floor(textWidth - barcodeWidth);
-		}
-	}
+	// fix : use barcodeWidth surely
+	// if(options.displayValue && barcodeWidth < textWidth){
+	// 	if(options.textAlign == "center"){
+	// 		return Math.floor((textWidth - barcodeWidth) / 2);
+	// 	}
+	// 	else if(options.textAlign == "left"){
+	// 		return 0;
+	// 	}
+	// 	else if(options.textAlign == "right"){
+	// 		return Math.floor(textWidth - barcodeWidth);
+	// 	}
+	// }
 	return 0;
 }
 
